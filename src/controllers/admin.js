9c95c13d444f63ca8a9f53fab02c94c446ffaae9
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2023-10-31 18:48:04 +0530

    Update admin.js

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -3,17 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-//const path = require("path");
 const User = require("../model/userModel");
 const db = require("../config/db");
 const Article = require("../model/articlePost");
-//const Comment = require("./model/commentModel");
+const Contact = require("../model/userContact");
+const BreakingNews = require('../model/breakingNews');
 
 const checkAuth = require("../middleware/checkauth");
 
 const router = express.Router();
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+router.get("/submitbreakingnews",  checkAuth, (req, res) => {
+  res.render("breakingnewspage");
+})
 
 router.get("/adminuser", checkAuth, async (req, res) => {
   try {
@@ -87,6 +91,41 @@ router.get("/viewpendingarticle/:urlTitle", checkAuth, async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Error fetching article");
+  }
+});
+
+router.get('/message', checkAuth, async (req, res) => {
+  try {
+    const messages = await Contact.find();
+    res.render('messages', { messages }); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching messages');
+  }
+});
+
+router.post('/breaking-news', checkAuth, async (req, res) => {
+  try {
+    const decodedToken = jwt.decode(req.cookies.token); 
+    if (!decodedToken) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    const username = decodedToken.username;
+    const user = await User.findOne({ username });
+    if(!user.isAdmin){
+      return res.send("you are not authorized to access this");
+    }
+    const { title} = req.body;
+    const breakingNews = new BreakingNews({ 
+      title, 
+      admin: username,
+    });
+    await breakingNews.save();
+    res.status(201).send("breaking news submitted successfully.. go back");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error creating breaking news' });
   }
 });
 
