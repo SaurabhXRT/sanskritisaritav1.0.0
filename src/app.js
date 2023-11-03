dbfb668a20daedb992e2d531a1d50822cfff9446
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2023-11-04 00:51:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cloudinary = require("cloudinary").v2;
 const app = express();
 const fileupload = require("express-fileupload");
 app.use(fileupload({ useTempFiles: true }));
-// const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 3000;
@@ -18,9 +17,6 @@ const Article = require("./model/articlePost");
 const Comment = require("./model/commentModel");
 
 // const SecretKey = require("./config/crypto");
-
-// // Define a secret key for JWT
-// const jwtSecret = SecretKey ;
 
 app.use((req, res, next) => {
   res.setHeader("X-Content-Type-Options", "nosniff");
@@ -38,23 +34,13 @@ cloudinary.config({
   api_secret: "d0UW2ogmMnEEMcNVcDpzG33HKkY",
 });
 
-// Custom helper function to format the date as "dd/mm/yyyy"
 hbs.registerHelper("formatDate", (date) => {
   const formattedDate = new Date(date).toLocaleDateString("en-GB");
   return formattedDate;
 });
 
-// Set up express-session
-// app.use(
-//   session({
-//     secret: jwtSecret,
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-
-// const adminUsername = "gautamkumar";
-// const adminPassword = "hellogautam";
+// const adminUsername = "";
+// const adminPassword = "";
 
 // (async () => {
 //   try {
@@ -78,16 +64,13 @@ hbs.registerHelper("formatDate", (date) => {
 //   }
 // })();
 
-app.use(express.static(path.join(__dirname, "../public")));
-// app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 const template_path = path.join(__dirname, "../views");
-
 app.set("views", template_path);
 app.set("view engine", "hbs");
 
@@ -108,102 +91,24 @@ app.use("/admin", adminController);
 const messageController = require("./controllers/message");
 app.use("/message", messageController);
 
-// const homeController = require("./controllers/admin");
-// app.use("/home", homeController);
+const indexController = require("./controllers/index");
+app.use("/", indexController);
 
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
+
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
 app.get("/article", checkAuth, (req, res) => {
   res.render("articlewrite");
 });
 
-
-
-// app.post("/like-post/:id", checkAuth, async  (req, res) => {
-//   const articleId = req.params.id;
-
-//   try {
-//     const post = await Article.findById(articleId);
-//     if (!post) {
-//       return res.status(404).send("article not found");
-//     }
-//     if (!res.locals.username) {
-//       // User is not logged in, handle as desired (redirect or error response)
-//       return res.redirect("/login");
-//   }
-
-//     const decodedToken = jwt.decode(req.cookies.token); 
-//     const username = decodedToken.username
-//     const user = await User.findOne({ username });
-
-//     const userLikedPost = post.likes.some((like) => {
-//       return like.user.toString() === user._id.toString();
-//     });
-
-//     if (userLikedPost) {
-//       post.likes = post.likes.filter((like) => {
-//         return like.user.toString() !== user._id.toString();
-//       });
-//     } else {
-//       post.likes.push({
-//         user: user._id,
-//       });
-//     }
-
-//     await post.save();
-//     const likesCount = post.likes.length;
-//     console.log("like added");
-//     res.json({ likesCount });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Internal server error");
-//   }
-// });
-
-// app.post("/comment", checkAuth, async (req, res) => {
-//   try {
-
-//     const { comment, postid } = req.body;
-//     const decodedToken = jwt.decode(req.cookies.token);
-//     const username = decodedToken.username
-//     const userid = await User.findOne({ username });
-//     const user = userid._id;
-
-//     const commentNew = new Comment({
-//       comment,
-//       author: user,
-//     });
-//     await commentNew.save();
-
-//     const article = await Article.findById(postid);
-//     article.comments.push(commentNew._id);
-//     await article.save();
-
-//     console.log("comment added");
-
-//     // Send the response as JSON
-//     const userr = await User.findById(user);
-//     const response = {
-//       postid: postid,
-//       username: userr.username,
-//       comment: comment,
-//     };
-//     res.json(response);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("Error adding comment");
-//   }
-// });
-
-
-
-
-const indexController = require("./controllers/index");
-app.use("/", indexController);
+app.get("/privacy-policy", (req, res) =>{
+  res.render("privacypolicy");
+});
 
 //app listening
 app.listen(PORT, () => {
